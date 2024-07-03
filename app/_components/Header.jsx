e4ed4498d524774064b0193b71429318ca8aad9b
Tanu Kaushik <developertanukaushik@gmail.com>
2024-07-03 16:17:58 +0530

    compilation error and signin page ui problem

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,12 +1,31 @@
+"use client"
+
+
 import Image from 'next/image'
 import React from 'react'
 
-const Header = () => {
+import { UserButton, useUser } from '@clerk/nextjs'
+// import { Button } from '@/components/ui/button';
+
+
+
+function Header  () {
+  const {user,isSignedIn}=useUser();
   return (
     <div className="p-5 border-b shadow-sm">
-        <div>
-            <Image src={'/logo.svg'} width={100}  height={150} alt='logo'></Image>
-        </div>
+      <div className="flex items-center justify-between">
+        
+            <Image src={'/logo.svg'} width={180}  height={50} alt='logo'></Image>
+           
+            {isSignedIn?
+          <div className='flex items-center gap-5'>
+            {/* <Button varient="outline">Dashboard</Button>   */}
+            <UserButton/>
+            </div>:
+            // <Button>Get started</Button>
+            <div></div>
+          }
+      </div>
     </div>
   )
 }
